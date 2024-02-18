a6e9967084b45769f11e4ebe3ec49f9c1e6bc068
SrKain <134458062+SrKain@users.noreply.github.com>
2024-02-17 21:12:09 -0300

    em andamento.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { useState } from "react";
 
 export default function App() {
+  const [location, setLocation] = useState({});
+
+  function Locationer(evento) {
+    setLocation({cep: evento.target.value})
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <main className="bg-gray-950 w-screen h-screen text-lg text-white flex justify-center items-center">
+      <div className="flex flex-col gap-1">
+        <h1>Consulte seu CEP</h1>
+        <input
+          className="bg-gray-800 rounded-md border-2 text-white px-2"
+          onChange={evento=>Locationer(evento)}
+          placeholder="digite seu cep"
+          type="text"
+        />
+        <p>'CEP: ${location}'</p>
+      </div>
+    </main>
   );
 }
